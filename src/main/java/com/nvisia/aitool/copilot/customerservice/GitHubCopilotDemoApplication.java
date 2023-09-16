29434f27453cab171639d0792976d60446b1dc00
mhoffman <mike@michaelhoffmaninc.com>
2023-09-16 07:45:56 -0500

    Initial update

---
+++ src/main/java/com/nvisia/aitool/copilot/customerservice/GitHubCopilotDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nvisia.aitool.copilot.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubCopilotDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubCopilotDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nvisia/aitool/copilot/customerservice/GitHubCopilotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvisia.aitool.copilot.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubCopilotDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
